b2cecf2a6c9ab7cdd35d20cc34cc0305905eb1f9
VladyslavSH <manujlenkovlad2@gmail.com>
2018-06-20 16:25:13 +0300

    Work is changed

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -20,9 +20,42 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        string [] parser = null;
+        double num1, num2, rez;
+        int flag = 0;
         public MainWindow()
         {
             InitializeComponent();
+            foreach (var item in grid.Children)
+            {
+                if((item as Button) is Button)
+                {
+                    (item as Button).Click += MainWindow_Click;
+                }
+            }
         }
+
+        private void MainWindow_Click(object sender, RoutedEventArgs e)
+        {
+            if((sender as Button).Content.ToString() == "Delete")
+            {
+                textBlock.Undo();
+            }
+            else if ((sender as Button).Content.ToString() == "C") textBlock.Clear();
+            if ((sender as Button).Content.ToString() == "=")
+            {
+                parser[0] = textBlock.Text;
+                switch((sender as Button).Content.ToString())
+                {
+                    case "+": ; break;
+            } 
+            if ((sender as Button).Content.ToString() == "+") { flag = 1; num1 = Convert.ToInt32(textBlock.Text); }
+            if ((sender as Button).Content.ToString() == "-") { flag = 2; num1 = Convert.ToInt32(textBlock.Text); }
+            if ((sender as Button).Content.ToString() == "*") { flag = 3; num1 = Convert.ToInt32(textBlock.Text); }
+            if ((sender as Button).Content.ToString() == "/") { flag = 4; num1 = Convert.ToInt32(textBlock.Text); }
+            else textBlock.Text += (sender as Button).Content.ToString();
+        }
+        
+
     }
 }
