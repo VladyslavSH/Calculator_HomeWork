aad4b48b158fcca5e3801f27b1fcb018b27f18c3
VladyslavSH <manujlenkovlad2@gmail.com>
2018-06-20 20:52:40 +0300

    Work is finished

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -40,22 +40,25 @@ namespace WpfApp1
             {
                 textBlock.Undo();
             }
-            else if ((sender as Button).Content.ToString() == "C") textBlock.Clear();
+            else if ((sender as Button).Content.ToString() == "C") { textBlock.Clear(); num1 = 0; num2 = 0; textBox.Clear(); }
             else if ((sender as Button).Content.ToString() == "=")
             {
-                num2 = Convert.ToInt32(textBlock.Text);
+                num2 = Convert.ToDouble(textBlock.Text);
                 switch (flag)
                 {
                     case 1: rez = num1 + num2; break;
+                    case 2: rez = num1 - num2; break;
+                    case 3: rez = num1 * num2; break;
+                    case 4: rez = num1 / num2; break;
                 }
                 textBox.Text += textBlock.Text + " = " + rez;
                 textBlock.Clear();
                 num1 = 0; num2 = 0;
             }
-            else if ((sender as Button).Content.ToString() == "+") { flag = 1; num1 = Convert.ToInt32(textBlock.Text); textBox.Text = textBlock.Text + "+"; textBlock.Clear(); }
-            else if ((sender as Button).Content.ToString() == "-") { flag = 2; num1 = Convert.ToInt32(textBlock.Text); textBox.Text = textBlock.Text + "-"; textBlock.Clear(); }
-            else if ((sender as Button).Content.ToString() == "*") { flag = 3; num1 = Convert.ToInt32(textBlock.Text); textBox.Text = textBlock.Text + "*"; textBlock.Clear(); }
-            else if ((sender as Button).Content.ToString() == "/") { flag = 4; num1 = Convert.ToInt32(textBlock.Text); textBox.Text = textBlock.Text + "/"; textBlock.Clear(); }
+            else if ((sender as Button).Content.ToString() == "+") { flag = 1; num1 = Convert.ToDouble(textBlock.Text); textBox.Text = textBlock.Text + "+"; textBlock.Clear(); }
+            else if ((sender as Button).Content.ToString() == "-") { flag = 2; num1 = Convert.ToDouble(textBlock.Text); textBox.Text = textBlock.Text + "-"; textBlock.Clear(); }
+            else if ((sender as Button).Content.ToString() == "*") { flag = 3; num1 = Convert.ToDouble(textBlock.Text); textBox.Text = textBlock.Text + "*"; textBlock.Clear(); }
+            else if ((sender as Button).Content.ToString() == "/") { flag = 4; num1 = Convert.ToDouble(textBlock.Text); textBox.Text = textBlock.Text + "/"; textBlock.Clear(); }
 
             else textBlock.Text += (sender as Button).Content.ToString();
             Console.WriteLine(num1);
